0fb65b2c715bca061ac475abbfb3b198b3081cb9
jierui303 <jierui303@163.com>
2021-04-15 17:09:59 +0800

    xx

--- go/xm1/a1.go
+++ go/xm1/a1.go
@@ -14,30 +14,95 @@ var a int = 10
 
 func main() {
 
-	//字符串打印 可以配合使用 Println()和 Printf()来打印各种自己感兴趣的信息
-	fmt.Println("!... Hello World ...!")
+	variable_function() //变量
+	const_function()    //常量
 
-	//局部变量声明
-	b := 33
+}
 
-	//局部多变量声明
-	var demoInt32 int32 = 98
-	println(demoInt32, a, b)
+//变量
+func variable_function() int {
 
-	//局部多变量var声明
+	//局部变量声明方式
+	b := 33                  //局部变量声明
+	var demoInt32 int32 = 98 //局部多变量声明
 	var (
 		v1 int
 		v2 string
-		v3 int
-	)
+	) //局部多变量var声明
+
+	//字符串打印
+	//可以配合使用 Println()和 Printf()来打印各种自己感兴趣的信息
+	fmt.Println("!... Hello World ...!")
+	println(demoInt32, a, b)
+	//Printf()
+	fmt.Printf("字符串打印结果：%d \n", b)
+
 	//局部变量赋值
 	v1 = 12
 	v2 = "hhhshshs"
+	fmt.Println(v1, v2)
+
+	return 1
+}
+
+//常量
+func const_function() (ret int, err error) {
+	//常量单个定义
+	const APPLE_TYPE_0 int = iota
+	const APPLE_TYPE_1 int = iota
+	fmt.Println("常量单个定义", APPLE_TYPE_0, APPLE_TYPE_1)
+
+	const (
+		c0 = iota
+		c1 = iota
+		c2 = iota
+		c3 = iota
+	)
+	fmt.Println("常量批量定义", c0, c1, c2, c3)
+
+	const (
+		size int64 = 1024
+		eof        = -1 // 无类型整型常量
+	)
+	fmt.Println("无类型整形常量定义", size, eof)
+
+	//常量的多重赋值
+	const u, v float32 = 0, 3
+	const aa, bb, cc = 3, 4, "foo"
+	fmt.Println("常量多重赋值定义", u, v, aa, bb, cc)
+
+	return 1, nil
+}
+
+//数组array 函数
+func array_function() (ret int, err error) {
 
 	//数组变量声明
 	var arr1 = []int{1, 2, 3, 5, 6, 7, 8, 9}
-	// Go 语言内置的函数 len()来取字符串的长度
-	v3 = len(arr1)
-	fmt.Println(v1, v2, v3)
 
+	// Go 语言内置的函数 len()来取字符串的长度
+	var v3 int = len(arr1)
+	fmt.Println(arr1, v3)
+
+	return 1, nil
 }
+
+//切片slice 函数
+func slice_function() (ret int, err error) {
+
+	return 1, nil
+}
+
+//字典map 函数
+func map_function() (ret int, err error) {
+
+	return 1, nil
+}
+
+//控制语句示例函数
+func control_loop() (ret int, err error) {
+
+	return 1, nil
+}
+
+//函数参数传递
